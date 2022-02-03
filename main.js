3347e4fee824b440e59ea679d2ef4099f631cdc5
DimanaNikolova <dimanannikolova@gmail.com>
2022-02-03 17:00:45 +0200

    csv data changed, main removes csv header

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function openCsv(e) {
     var reader = new FileReader()
 
     reader.onload = function (e) {
-        var employees = e.target.result.split(/\n/)
+        var employees = e.target.result.slice(e.target.result.indexOf("\n") + 1).split("\n");
 
         let res = getOutput(employees)
 
@@ -13,7 +13,7 @@ function openCsv(e) {
     reader.readAsText(file)
 }
 
-function errorHandling(emp1, emp2) {
+function errorHandling(emp1,  emp2) {
     if (emp1.length !== 4 || emp2.length !== 4) {
         return 'Invalid length data'
     } else if (
@@ -29,14 +29,13 @@ function getOutput(employees) {
     let workedTogether = []
 
     employees.forEach((emp1) => {
-        emp1 = emp1.split(',')
+        emp1 = emp1.split(',  ')
 
         employees.forEach((emp2) => {
-            emp2 = emp2.split(',')
+            emp2 = emp2.split(',  ')
 
             if (emp1[0] !== emp2[0] && emp1[1] === emp2[1]) {
-
-                errors = errorHandling(emp1, emp2)
+                errors = errorHandling(emp1,  emp2)
 
                 let emp1Id = emp1[0]
                 let emp2Id = emp2[0]
@@ -46,26 +45,28 @@ function getOutput(employees) {
                 let startEmp2 = new Date(emp2[2])
 
                 let endEmp1 =
-                    emp1[3] === 'null' ? new Date() : new Date(emp1[3])
+                    emp1[3] === 'NULL' ? new Date() : new Date(emp1[3])
                 let endEmp2 =
-                    emp2[3] === 'null' ? new Date() : new Date(emp2[3])
+                    emp2[3] === 'NULL' ? new Date() : new Date(emp2[3])
 
                 let overlap = startEmp1 <= endEmp2 && startEmp2 <= endEmp1
 
                 if (overlap) {
-                    let start = Math.max(startEmp1, startEmp2)
-                    let end = Math.min(endEmp1, endEmp2)
+                    let start = Math.max(startEmp1,  startEmp2)
+                    let end = Math.min(endEmp1,  endEmp2)
 
-                    let period = Math.floor((end - start) / (1000 * 60 * 60 * 24))
+                    let period = Math.floor(
+                        (end - start) / (1000 * 60 * 60 * 24)
+                    )
 
-                    workedTogether.push([emp1Id, emp2Id, project, period])
+                    workedTogether.push([emp1Id,  emp2Id,  project,  period])
                 }
             }
         })
     })
 
     console.log(workedTogether)
-    let finalOutput = workedTogether.sort((a, b) => a[3] - b[3]).pop()
+    let finalOutput = workedTogether.sort((a,  b) => a[3] - b[3]).pop()
 
     return errors
         ? errors
